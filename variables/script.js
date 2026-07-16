7a9df5af5b4669036d8343807f622318ff793b27
Harshit Namdev <harshitnamdev2006@gmail.com>
2026-07-16 17:14:18 +0530

    introduction & variables

---
+++ variables/script.js
@@ -0,0 +1,10 @@
+console.log("welcome to variables")
+var variablename="hello";
+console.log(variablename) 
+const a=100;
+console.log(a);
+variablename="harshit";
+console.log(variablename);
+if(variablename=="harshit"){
+    console.log("yup");
+}
