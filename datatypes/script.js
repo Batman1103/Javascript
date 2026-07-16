543ff9693606e3656773091edcc33599fac2b3f4
Harshit Namdev <harshitnamdev2006@gmail.com>
2026-07-16 20:09:43 +0530

    new code

---
+++ datatypes/script.js
@@ -0,0 +1,12 @@
+var name_="Ramesh";
+console.log(name_);
+// this is single line comment
+/* this is  multiline comment */
+const logic=`hello ,${2+2}`;
+console.log(logic);
+var number=10;
+var decimalnumber=0.55;
+console.log(number);
+console.log(decimalnumber);
+console.log(typeof decimalnumber);
+// so basically the typeof is function which is use to determine the type of variable
