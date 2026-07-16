db9ed93dc85cd6d2814ad09b2aa02f7363e781b5
Harshit Namdev <harshitnamdev2006@gmail.com>
2026-07-16 20:38:45 +0530

    object

--- datatypes/script.js
+++ datatypes/script.js
@@ -1,12 +1,54 @@
+// String
 var name_="Ramesh";
 console.log(name_);
+console.log(typeof name_)
+
+// comments
+
 // this is single line comment
 /* this is  multiline comment */
+
+
+// Backticks
 const logic=`hello ,${2+2}`;
 console.log(logic);
+
+// Number
 var number=10;
 var decimalnumber=0.55;
+
 console.log(number);
 console.log(decimalnumber);
 console.log(typeof decimalnumber);
+
+// typeof
 // so basically the typeof is function which is use to determine the type of variable
+
+
+// Booleans
+var age=true
+console.log(age);
+console.log(typeof age);
+
+// Null
+var last=null;
+console.log(last);
+console.log(typeof last);
+// type of null is an object it's the bug in the javascript
+// this bug was not fixed because large server and company
+// are denpended on this and their code will crash.
+console.log(typeof null);
+// undefined
+// A variable in which value is not define that's the undefined variable.
+var x;
+console.log(x);
+
+// object
+var person={
+    a:"harshit",
+    b:20
+
+};
+console.log(person.a);
+console.log(person.b);
+console.log(person);
